81bc427404e41de7ea9f0da9095339d499a3e734
zhiyenbek <zkantay@wtotem.net>
2022-09-25 00:19:34 +0600

    feature: add migrations

---
+++ scripts/init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS users (
+  id bigserial,
+  name text NOT NULL ,
+  _created_at timestamp DEFAULT now() NOT NULL ,
+  PRIMARY KEY(id)
+);
+CREATE TABLE IF NOT EXISTS positions (
+  id bigserial,
+  latitude float NOT NULL ,
+  longitude float NOT NULL ,
+  altitude float NOT NULL ,
+  floorLabel text ,
+  activity text ,
+  h_accuracy float NOT NULL,
+  v_accuracy float NOT NULL,
+  _created_at timestamp DEFAULT now() NOT NULL ,
+   user_id bigint,
+  PRIMARY KEY(id),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
