c1ad592afa4f9d78c7cbbb0fa1bcff39e7a66db3
DulatKuntu <dulaizzhambula@gmail.com>
2022-09-25 04:13:58 +0600

    asd

--- model/location.go
+++ model/location.go
@@ -9,8 +9,8 @@ type LocationData struct {
 	Identifier         string  `json:"identifier"`
 	Timestamp          int64   `json:"timestamp"`
 	FloorLabel         string  `json:"floor"`
-	HorizontalAccuracy int     `json:"horizontalAccuracy"`
-	VerticalAccuracy   int     `json:"verticalAccuracy"`
+	HorizontalAccuracy float64 `json:"horizontalAccuracy"`
+	VerticalAccuracy   float64 `json:"verticalAccuracy"`
 	Activity           string  `json:"activity"`
 	UserID             int64   `json:"userID"`
 }
