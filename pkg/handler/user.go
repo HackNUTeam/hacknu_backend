4b482538fc20ee29b3bd05e8e4a89b52d19853b8
DulatKuntu <dulaizzhambula@gmail.com>
2022-09-25 01:41:03 +0600

    asd

--- pkg/handler/user.go
+++ pkg/handler/user.go
@@ -7,28 +7,8 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/gin-gonic/gin"
-)
-
-const (
-	// Time allowed to write a message to the peer.
-	writeWait = 10 * time.Second
-
-	// Time allowed to read the next pong message from the peer.
-	pongWait = 60 * time.Second
-
-	// Send pings to peer with this period. Must be less than pongWait.
-	pingPeriod = (pongWait * 9) / 10
-
-	// Maximum message size allowed from peer.
-	maxMessageSize = 512
-)
-
-var (
-	newline = []byte{'\n'}
-	space   = []byte{' '}
 )
 
 func (h *Handler) serveHome(c *gin.Context) {
@@ -115,7 +95,7 @@ func (h *Handler) listenUser(client *model.Client) {
 			var location model.LocationData
 			err := json.Unmarshal(msg, &location)
 			if err != nil {
-				log.Print(errors.New("Could not unmarshall"))
+				log.Print(errors.New("could not unmarshall"))
 				return
 			}
 			err = h.services.User.CreateReading(&location)
